47978a4369a9a6e863d904c4d36df1670d7ad7a5
Eleonor <elidoescode@gmail.com>
2023-11-16 12:36:00 +0100

    start av uppbyggnad

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './views/Home'
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+
+    </BrowserRouter>
   )
 }
 
---
+++ src/views/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '@components/header'
+import Footer from '@components/Footer'
+import Showcase from '@components/Showcase'
+
+const Home = () => {
+  return (
+    <>
+        <Header />
+        <Showcase />
+        <Footer />
+    </>
+    
+  )
+}
+
+export default Home
